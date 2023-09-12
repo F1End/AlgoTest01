a74a5da479f6b662d0d2f2c1df46264bebe8cba2
F1End <horvirich@gmail.com>
2023-09-12 20:32:20 +0200

    -Created initial setup for testing basics with Pandas matrix construction

--- algo2.py
+++ algo2.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from typing import List
+
+import pandas as pd
+import numpy as np
 
 
 class Client:
@@ -9,8 +11,8 @@ class Client:
 
 
 class Provider:
-    def __init__(self, preffered_avaibility):
-        self.avaibility = preffered_avaibility
+    def __init__(self, prefered_avaibility):
+        self.avaibility = prefered_avaibility
 
 
 class TimeMatrix:
@@ -20,14 +22,43 @@ class TimeMatrix:
         self.matrix = self.construct_matrix()
 
     def construct_matrix(self):
-        df = pd.DataFrame.columns = self.provider.avaibility
-        print(self.clients)
-        df.insert(0, "client")
-        df.insert(1, "client_name")
+        columns = ["client", "client_name"]
+        columns.extend(self.provider.avaibility)
+        print(self.provider.avaibility)
+        print(columns)
+        rows = [self.initiate_rows(clnt) for clnt in self.clients]
+        df = pd.DataFrame(rows, columns=columns)
         return df
 
+    def initiate_rows(self, client):
+        row_data = [client, client.name]
+        for timeslot in self.provider.avaibility:
+            if timeslot in client.avaibility:
+                row_data.append("1")
+            else:
+                row_data.append("0")
+        return row_data
+
     def construct_row(self, client):
-        self.matrix.add
+        """
+        For clients added later, not in initial construction.
+        :param client:
+        :return:
+        """
+        row = {col: 0 for col in self.matrix.columns}
+        row["client"] = client
+        row["client_name"] = client.name
+        for col in self.matrix.columns:
+            if col in client.avaibility:
+                pass
+        # self.matrix = self.matrix.append(row, ignore_index=True)
+
+    def update_row(self, client):
+        """
+        For data update on already added clients
+        :param client:
+        :return:
+        """
 
 
 provider_avaibility = ["Mon: 8", "Mon: 10", "Mon: 12", "Tue: 8", "Tue: 10", "Tue: 12", "Wed: 10", "Wed: 12"]
@@ -42,3 +73,6 @@ clients = [client1, client2, client3, client4]
 
 calendar = TimeMatrix(provider, clients)
 print(calendar.matrix)
+
+
+print(calendar.matrix.to_string())
